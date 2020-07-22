c179cc52129b772ea4737f95fc2a0c2545ac0c9c
Daryl Metzler <daryl@ifixit.com>
2020-07-22 08:30:50 -0700

    Fix indentation
    We want to use the idiomatic 2 spaces for indents. A lot of these had
    5. The JSON arrays are still a little off, but the code structure now
    doesn't set off any indentation cops.

--- pullbot.rb
+++ pullbot.rb
@@ -30,146 +30,146 @@ $edge_color_merged = config['slack']['edge_colors']['merged']
 $edge_color_closed = config['slack']['edge_colors']['closed']
 
 helpers do
-     def verify_signature(payload_body, signature_from_header)
-          signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, $gh_secret, payload_body)
-          return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, signature_from_header)
-     end
+  def verify_signature(payload_body, signature_from_header)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, $gh_secret, payload_body)
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, signature_from_header)
+  end
 
-     def make_json(repo_meta, pr, author_meta, action, sender_meta, pr_icon_uri, attachment, edge_color)
-          j = {
-               'blocks' => [
-                    {
-                         'type' => 'divider'
-                    },
-                    {
-                         'type' => 'context',
-                         'elements' => [
-                              {
-                                   'type' => 'image',
-                                   'image_url' => "#{pr_icon_uri}",
-                                   'alt_text' => "#{action}"
-                              },
-                              {
-                                   'type' => 'mrkdwn',
-                                   'text' => "Pull request #{action}"
-                              }
-                         ]
-                    },
+  def make_json(repo_meta, pr, author_meta, action, sender_meta, pr_icon_uri, attachment, edge_color)
+    j = {
+        'blocks' => [
+             {
+                  'type' => 'divider'
+             },
+             {
+                  'type' => 'context',
+                  'elements' => [
+                       {
+                            'type' => 'image',
+                            'image_url' => "#{pr_icon_uri}",
+                            'alt_text' => "#{action}"
+                       },
+                       {
+                            'type' => 'mrkdwn',
+                            'text' => "Pull request #{action}"
+                       }
+                  ]
+             },
+             {
+                  'type' => 'section',
+                  'text' => {
+                       'type' => 'mrkdwn',
+                       'text' => "*[<#{repo_meta['html_url']}|#{repo_meta['full_name']}>]*"\
+                                 " <#{pr['html_url']}|_#{pr['title']}_ *##{pr['number']}*>"
+                  }
+             },
+             {
+                  'type' => 'context',
+                  'elements' => [
+                       {
+                            'type' => 'image',
+                            'image_url' => author_meta['avatar_url'],
+                            'alt_text' => author_meta['login']
+                       },
+                       {
+                            'type' => 'mrkdwn',
+                            'text' => "by <#{author_meta['html_url']}|#{author_meta['login']}>"
+                       },
+                       {
+                            'type' => 'image',
+                            'image_url' => sender_meta['avatar_url'],
+                            'alt_text' => sender_meta['login']
+                       },
+                       {
+                            'type' => 'mrkdwn',
+                            'text' => " #{action} by <#{sender_meta['html_url']}|#{sender_meta['login']}>"
+                       }
+                  ]
+             },
+             {
+                  'type' => 'divider'
+             }
+        ]
+   }
+   if (attachment)
+     j['attachments'] = [
+          {
+          'color' => "#{edge_color}",
+          'blocks' => [
                     {
                          'type' => 'section',
                          'text' => {
                               'type' => 'mrkdwn',
-                              'text' => "*[<#{repo_meta['html_url']}|#{repo_meta['full_name']}>]*"\
-                                        " <#{pr['html_url']}|_#{pr['title']}_ *##{pr['number']}*>"
+                              'text' => "#{pr['body']}"
                          }
-                    },
-                    {
-                         'type' => 'context',
-                         'elements' => [
-                              {
-                                   'type' => 'image',
-                                   'image_url' => author_meta['avatar_url'],
-                                   'alt_text' => author_meta['login']
-                              },
-                              {
-                                   'type' => 'mrkdwn',
-                                   'text' => "by <#{author_meta['html_url']}|#{author_meta['login']}>"
-                              },
-                              {
-                                   'type' => 'image',
-                                   'image_url' => sender_meta['avatar_url'],
-                                   'alt_text' => sender_meta['login']
-                              },
-                              {
-                                   'type' => 'mrkdwn',
-                                   'text' => " #{action} by <#{sender_meta['html_url']}|#{sender_meta['login']}>"
-                              }
-                         ]
-                    },
-                    {
-                         'type' => 'divider'
                     }
                ]
           }
-          if (attachment)
-               j['attachments'] = [
-                    {
-                    'color' => "#{edge_color}",
-                    'blocks' => [
-                              {
-                                   'type' => 'section',
-                                   'text' => {
-                                        'type' => 'mrkdwn',
-                                        'text' => "#{pr['body']}"
-                                   }
-                              }
-                         ]
-                    }
-               ]
-          end
-          j.to_json
-     end
+     ]
+   end
+   j.to_json
+  end
 
-     def notify_json(to_uri, json_payload)
-          Net::HTTP.post to_uri,
-               json_payload,
-               'Content-Type' => 'application/json'
-     end
+  def notify_json(to_uri, json_payload)
+    Net::HTTP.post to_uri,
+      json_payload,
+      'Content-Type' => 'application/json'
+  end
 end
 
 post '/payload' do
-     request.body.rewind
-     payload_body = request.body.read
-     body = JSON.parse payload_body
+  request.body.rewind
+  payload_body = request.body.read
+  body = JSON.parse payload_body
 
-     verify_signature(payload_body, request.env['HTTP_X_HUB_SIGNATURE'])
+  verify_signature(payload_body, request.env['HTTP_X_HUB_SIGNATURE'])
 
-     # PR action details
-     action = body['action']
+  # PR action details
+  action = body['action']
 
-     # Get the PR details
-     pr = body['pull_request']
-     merged =  pr['merged']
+  # Get the PR details
+  pr = body['pull_request']
+  merged =  pr['merged']
 
-     pr_link = pr['html_url']
-     repo_meta = body['repository']
-     author_meta = pr['user']
-     sender_meta = body['sender']
+  pr_link = pr['html_url']
+  repo_meta = body['repository']
+  author_meta = pr['user']
+  sender_meta = body['sender']
 
-     # Whether to show the PR description
-     attachment = false
+  # Whether to show the PR description
+  attachment = false
 
-     # PR opened
-     if action == 'opened'
-          webhook_uri = $opened_uri
-          icon_uri = $pr_icon_opened_uri
-          color = $edge_color_opened
-          attachment = true
-          response = 'Opened PR message receieved'
-     # PR re-opened
-     elsif action == 'reopened'
-          action = 'reopened'
-          webhook_uri = $opened_uri
-          icon_uri = $pr_icon_opened_uri
-          color = $edge_color_opened
-          response = 'Reopened PR message receieved'
-     # PR merged
-     elsif (action == 'closed') && merged
-          action = 'merged'
-          webhook_uri = $merged_uri
-          icon_uri = $pr_icon_merged_uri
-          color = $edge_color_merged
-          response = 'Merged PR message receieved'
-     # PR closed without merging
-     elsif (action == 'closed') && !merged
-          action = 'closed without merge'
-          webhook_uri = $closed_uri
-          icon_uri = $pr_icon_closed_uri
-          color = $edge_color_clsoed
-          response = 'Closed PR message receieved'
-     end
-     notify_json webhook_uri,
-          make_json(repo_meta, pr, author_meta, action, sender_meta, icon_uri, attachment, color)
-     response
+  # PR opened
+  if action == 'opened'
+    webhook_uri = $opened_uri
+    icon_uri = $pr_icon_opened_uri
+    color = $edge_color_opened
+    attachment = true
+    response = 'Opened PR message receieved'
+  # PR re-opened
+  elsif action == 'reopened'
+    action = 'reopened'
+    webhook_uri = $opened_uri
+    icon_uri = $pr_icon_opened_uri
+    color = $edge_color_opened
+    response = 'Reopened PR message receieved'
+  # PR merged
+  elsif (action == 'closed') && merged
+    action = 'merged'
+    webhook_uri = $merged_uri
+    icon_uri = $pr_icon_merged_uri
+    color = $edge_color_merged
+    response = 'Merged PR message receieved'
+  # PR closed without merging
+  elsif (action == 'closed') && !merged
+    action = 'closed without merge'
+    webhook_uri = $closed_uri
+    icon_uri = $pr_icon_closed_uri
+    color = $edge_color_clsoed
+    response = 'Closed PR message receieved'
+  end
+  notify_json webhook_uri,
+    make_json(repo_meta, pr, author_meta, action, sender_meta, icon_uri, attachment, color)
+  response
 end
 
